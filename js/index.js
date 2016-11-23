8dbdd613e228596a06bec16c00272b9cf361df46
AxelGoetz <axel.goetz@hotmail.com>
2016-11-23 19:53:51 +0000

    Initial object structure

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Every object has the following structure:
+ * object {
+ *   mass: Double,
+ *   radius, Double
+ *   velocity: [x, y],
+ *   force: [x, y],
+ *   coord: [x, y],
+ *   lastTimeUpdated: Int (Date.now())
+ * }
+ *
+ * The reason we use lists is because we might expand it to more dimensions
+ */
